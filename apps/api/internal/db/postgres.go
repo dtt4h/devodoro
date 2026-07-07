5470a22852c34b4ac5bfa77e317c982f6b14b5f4
dtt4h <dtt4h322@gmail.com>
2026-07-07 20:16:40 +0700

    feat: init db connection pool

--- apps/api/internal/db/postgres.go
+++ apps/api/internal/db/postgres.go
@@ -1,5 +1,17 @@
 package db
 
-func NewPool() (*pgxpool.Pool, error) {
+import (
+	"context"
 
+	"github.com/dtt4h/devodoro/internal/config"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool(ctx context.Context, cfg config.Database) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(ctx, cfg.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
 }
