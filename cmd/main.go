3a7b9d2ec0b7f7dc78aca51221f564535d1e4f36
dtt4h <dtt4h322@gmail.com>
2026-09-21 18:43:05 +0700

    feat: init basic main

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	log.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", r)
+}
