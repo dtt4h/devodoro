3a2fe958a2945cfc9fa9276288e35ddc078b79fc
dtt4h <dtt4h322@gmail.com>
2026-09-21 18:18:24 +0700

    feat(db): create pool to connect db

---
+++ internal/database/postgres.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/dtt4h/devodoro/internal/config"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool(ctx context.Context, cfg config.DatabaseConfig) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(ctx, cfg.URL)
+	if err != nil {
+		return nil, fmt.Errorf("create pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+
+		return nil, fmt.Errorf("ping postgres: %w", err)
+	}
+
+	return pool, err
+}
